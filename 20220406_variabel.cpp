4ce9c86e2b1c5afb7c0233a226020b1635e961b5
clarr23 <103086051+clarr23@users.noreply.github.com>
2022-04-06 11:08:50 +0800

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,11 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+float angka;
+string nama; //deklarasi variabel
+nama = "Clarissa"; //masukkan value kedalam variabel
+angka = 10.21+3.14;
+cout<<nama<<endl; //menampilkan variabel
+cout<<angka;
+}//end of function
