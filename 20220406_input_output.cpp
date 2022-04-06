7fb41cd771d855e7493d8718c7e4a8de0149475e
clarr23 <103086051+clarr23@users.noreply.github.com>
2022-04-06 11:27:08 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,8 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+string nama;
+cout<<"Masukkan Nama: "; cin>>nama;
+cout<<"Nama Anda: "<<nama;
+}//end of function
